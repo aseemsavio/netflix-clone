b189d490ecb6df31a81ffe1bb2cb74cf41f4ab96
aseemsavio <aseemsavio3@gmail.com>
2021-01-11 22:56:29 +0530

    trailers added

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react'
+import './Navbar.css';
+
+function Navbar() {
+
+    const[show, handleShow] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else {
+                handleShow(false);
+            }
+        });
+        return () => {
+            window.removeEventListener("scroll")
+        };
+    }, []);
+
+    return (
+        <div className={`navbar ${show && "nav__black"}`}>
+            <img 
+                className="navbar__logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+                alt="Netflix Logo"
+            />
+            <img 
+                className="navbar__avatar"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+                alt="User Avatar"
+            />
+        </div>
+    )
+}
+
+export default Navbar
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import axios from '../axios'
 import './Row.css'
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 
 function Row({ title, fetchUrl, isLargeRow }) {
 
     const [movies, setmovies] = useState([]);
     const baseUrl = "https://image.tmdb.org/t/p/original/"
+    const [trailerUrl, setTrailerUrl] = useState("");
+
+    const youtubeOptions = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
 
     // fetchUrl is added as a dependency in useEffect as it should be.
     // When ever we use an outside variable inside useEfefct, we need to mention that
@@ -20,6 +31,19 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchData();
     }, [fetchUrl]);
 
+    const handleClick = (movie) => {
+        // This will close out the youtube video that is already playing.
+        if (trailerUrl) {
+            setTrailerUrl("")
+        } else {
+            movieTrailer(movie?.name || "")
+            .then((url) => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));
+            }).catch((error) => console.log(error))
+        }
+    }
+
     return (
         <div className="row">
             <h3>{title}</h3>
@@ -27,12 +51,14 @@ function Row({ title, fetchUrl, isLargeRow }) {
                 {movies.map(movie => {
                     return (<img
                         key={movie.id}
+                        onClick={() => handleClick(movie)}
                         className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                         src={`${baseUrl}${isLargeRow ? movie.poster_path: movie.backdrop_path}`}
                         alt={movie.name}
                     />);
                 })}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={youtubeOptions}/>}
         </div>
     )
 }
